23e485495bd06bb740ae8cb46d92408f7c16785f
solufemi91 <stevenoo@spartaglobal.com>
2018-01-22 19:33:20 +0000

    finished step defs for sign in

---
+++ UI-test/features/sign_in_scenario_outline.feature
@@ -0,0 +1,10 @@
+Feature: Sign in
+
+  Scenario: Sign from the homepage
+
+  Given I am on the homepage
+  And I have clicked on the sign in link
+  When i enter my github account details
+  And press enter
+  Then I am redirected to the dashbord page which includes my name as a link
+  And a sidebar on the left hand side
---
+++ UI-test/features/step_definitions/sign_in_step_defs.rb
@@ -0,0 +1,34 @@
+Given("I am on the homepage") do
+  homepage.visit_home_page
+  sleep 1
+end
+
+Given("I have clicked on the sign in link") do
+  sign_in_page.sign_in_link
+  sleep 1
+end
+
+
+When("i enter my github account details") do
+  sign_in_page.enter_github_username
+  sleep 1
+  sign_in_page.enter_github_psswrd
+  sleep 1
+end
+
+And("press enter") do
+  sign_in_page.confirm_github_details
+  sleep 1
+end
+
+Then("I am redirected to the dashbord page which includes my name as a link") do
+  dashboard_page.find_dashboard_title
+  sleep 1
+  dashboard_page.find_name_link
+  sleep 1
+end
+
+And("a sidebar on the left hand side") do
+  navbar.click_menu
+  sleep 4
+end
